32571f5cf3a57a778dd027fe08b9ed2d3ce1d984
Aman Dhillon <a.dhillon9779@gmail.com>
2023-07-15 00:34:47 +0530

    MyClient class made

---
+++ src/MyClient.java
@@ -0,0 +1,26 @@
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import javax.swing.JOptionPane;
+
+
+public class MyClient 
+{
+    static String fetchIndex()
+    {
+        try
+        {
+        String url="http://localhost:9000/";
+        HttpResponse<String> response= Unirest.get(url).asString();
+        String ans = response.getBody();
+        return ans;
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            return ex.toString();
+
+        }
+    }
+        
+}
---
+++ src/MyClient_Testing.java
@@ -0,0 +1,8 @@
+
+public class MyClient_Testing
+{
+    public static void main(String[] args) {
+        String ans1= MyClient.fetchIndex();
+        System.out.println(ans1);
+    }
+}
